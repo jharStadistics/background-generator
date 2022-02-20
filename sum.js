849b0b53789d95666c102eb60f082d8a605445ef
juanx <juanhar865@gmail.com>
2022-02-20 21:41:55 +0100

    a new feature in sum.js

--- sum.js
+++ sum.js
@@ -1,4 +1,8 @@
 //proof
 const num=[1,2,3,4,5];
+const  letter=['a','b','c','d'];
+
 const sum = num.reduce((a,b)=> a+b);
 console.log(`the total is ${sum}`);
+
+console.log(`the quantity of letter is ${letter.length}`);
